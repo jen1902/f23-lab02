31f20561442a17aaf4ab2bab3f8dc5e414884cdd
Jennifer Wang <1155136569@link.cuhk.edu.hk>
2023-09-05 23:48:10 -0400

    Update Renderer.java
    encapsulate shape info in Renderer

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,17 +3,24 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
+    // public Rectangle rectangle;
+    public Shape shape;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    // Renderer(Rectangle rectangle) {
+    //     this.rectangle = rectangle;
+    // }
+    Renderer(Shape shape) {
+        // this.rectangle = rectangle;
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
+        // double area = rectangle.getArea();
+        String shapename = shape.getName();
+        double area = shape.getArea();
 
         // assume implementation
 
-        System.out.println("Shape printed\n" + "Its area is " + area);
+        System.out.println("Shape printed: " + shapename + "\nIts area is " + area);
     }
 }
