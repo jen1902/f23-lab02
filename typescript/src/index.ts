53a2171ee23e4de0f599e5d6c9293a8b4204c9e3
Jennifer Wang <1155136569@link.cuhk.edu.hk>
2023-09-06 00:31:47 -0400

    Update index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,12 @@
 import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+// import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import {Shape} from './shapes/shape'
+import { newRectangle } from "./shapes/rectangle.js";
+import { newCircle } from "./shapes/circle.js";
 
-const rectangle: Rectangle = newRectangle(2, 3)
-const renderer = newRenderer(rectangle)
+// const rectangle: Rectangle = newRectangle(2, 3)
+const rectangle: Shape = newRectangle(2, 3)
+const circle: Shape = newCircle(3)
+
+const renderer = newRenderer(circle)
 renderer.draw();
