9b2da26be1b5359f5d4ae64f98fdecf4105f33f3
Jennifer Wang <1155136569@link.cuhk.edu.hk>
2023-09-05 23:49:05 -0400

    Update Main.java
    declare shapes (rectangle, circle, square) as Shape

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,12 +1,15 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+// import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(3);
+        Shape square = new Square(6);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer = new Renderer(square);   // No longer needs to provide Rectangle to Render?
 
         renderer.draw();
     }
